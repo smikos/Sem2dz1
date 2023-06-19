d41f148eecdcebd11ceec1ec836b919feff010c6
Konstantin Smirnov <sasharugby@yandex.ru>
2023-06-19 19:26:02 +0300

    work good

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write( message );
+    int value= Convert.ToInt32(Console.ReadLine());
+    return value ;
+}
+
+int number = Prompt("Введите трехзначное число >");
+if (number<100 || number >=1000)
+
+{
+    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"Введенное число '{number}'");
+int secondRank = number /10%10;
+Console.WriteLine($"Вторая цифра '{secondRank}'");
